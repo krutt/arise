c664042d8d6847b32460ecd7a496ea03b62404c3
Sitt Guruvanich <aekazitt@gmail.com>
2024-08-24 02:45:36 +0700

    chore: version bump; 0.1.1 -> 0.1.2

--- src/arise/types/build.py
+++ src/arise/types/build.py
@@ -2,7 +2,7 @@
 # coding:utf-8
 # Copyright (C) 2024 All rights reserved.
 # FILENAME:    ~~/src/arise/types/build_enum.py
-# VERSION:     0.1.1
+# VERSION:     0.1.2
 # CREATED:     2024-07-07 16:11
 # AUTHOR:      Sitt Guruvanich <aekasitt.g+github@siamintech.co.th>
 # DESCRIPTION:
