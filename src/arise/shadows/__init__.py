21d714bec043008b9d114dcfccd40e1e6d61a4c4
Sitt Guruvanich <aekazitt@gmail.com>
2024-08-31 18:48:33 +0700

    chore: version bump; 0.1.5 -> 0.1.6

--- src/arise/shadows/__init__.py
+++ src/arise/shadows/__init__.py
@@ -2,7 +2,7 @@
 # coding:utf-8
 # Copyright (C) 2024 All rights reserved.
 # FILENAME:    ~~/src/arise/shadows/__init__.py
-# VERSION:     0.1.5
+# VERSION:     0.1.6
 # CREATED:     2024-07-07 16:11
 # AUTHOR:      Sitt Guruvanich <aekasitt.g+github@siamintech.co.th>
 # DESCRIPTION: https://www.w3docs.com/snippets/python/what-is-init-py-for.html
