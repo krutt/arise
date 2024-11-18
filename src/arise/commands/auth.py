5f082a35eec913cdbf33e173b44e2848cdbfc6c7
Sitt Guruvanich <aekazitt@gmail.com>
2024-11-18 15:33:42 +0700

    feat: authentications via rc files

--- src/arise/commands/auth.py
+++ src/arise/commands/auth.py
@@ -13,7 +13,7 @@
 ### Standard packages ###
 from os import getenv, path
 from re import findall
-from typing import Dict, List
+from typing import Dict, List, Optional, Tuple
 
 ### Third-party packages ###
 from click import argument, command, option
@@ -23,11 +23,14 @@ from rich import print as rich_print
 
 
 @command
-@argument("rpcuser", nargs=1)
+@argument("rpcuser", required=False)
+@argument("dbuser", required=False)
+@argument("dbpass", required=False)
 @option("--bash", is_flag=True, help="Persist authentications in .bashrc", type=bool)
-@option("--fish", is_flag=True, help="Persist authentications in .fishrc", type=bool)
 @option("--zsh", is_flag=True, help="Persist authentications in .zshrc", type=bool)
-def auth(bash: bool, fish: bool, rpcuser: str, zsh: bool) -> None:
+def auth(
+  bash: bool, dbpass: Optional[str], dbuser: Optional[str], rpcuser: Optional[str], zsh: bool
+) -> None:
   """Persist authentications in desired run-control file."""
   client: DockerClient
   try:
@@ -37,30 +40,36 @@ def auth(bash: bool, fish: bool, rpcuser: str, zsh: bool) -> None:
   except DockerException:
     rich_print("[red bold]Unable to connect to docker daemon.")
     return
-  shell_select: Dict[str, bool] = {"/bin/bash": bash, "/bin/fish": fish, "/bin/zsh": zsh}
+  shell_select: Dict[str, bool] = {"/bin/bash": bash, "/bin/zsh": zsh}
   shell: str = getenv("SHELL", "/bin/bash")
   try:
     shell = next(filter(lambda value: value[1], shell_select.items()))[0]
   except StopIteration:
     pass
-  # TODO: validate rpcuser
-  rc_path: str = {"/bin/bash": "~/.bashrc", "/bin/fish": "~/.fishrc", "/bin/zsh": "~/.zshrc"}[shell]
-  authenticated: bool = False
-  if getenv("ARISE_AUTH_RPCUSER", None) is not None:
-    authenticated = True
-  else:
-    with open(path.expanduser(rc_path), "r") as rc_readonly:
-      found: List[str] = findall(r"export ARISE_AUTH_RPCUSER=", rc_readonly.read())
-      authenticated = len(found) != 0
-  if authenticated:
-    rich_print("[yellow]Already authenticated![reset]")
-    return
-  if rpcuser is not None:
-    with open(path.expanduser(rc_path), "a") as rc_output:
-      rc_output.write(f"export ARISE_AUTH_RPCUSER={ rpcuser }\n")
-  rich_print("[green]Success!")
-  rich_print(f"In order to activate authentication, run the following command:")
-  rich_print(f"    [blue]source { path.expanduser(rc_path) }[reset]")
+  outputs: List[str] = []
+  rc_path: str = {"/bin/bash": "~/.bashrc", "/bin/zsh": "~/.zshrc"}[shell]
+  keys: Tuple[str, ...] = ("ARISE_AUTH_DBPASS", "ARISE_AUTH_DBUSER", "ARISE_AUTH_RPCUSER")
+  write_count: int = 0
+  for key, value in zip(keys, (dbpass, dbuser, rpcuser)):
+    authenticated: bool = False
+    if getenv(key, None) is not None:
+      authenticated = True
+    else:
+      with open(path.expanduser(rc_path), "r") as rc_readonly:
+        found: List[str] = findall(f"export { key }=", rc_readonly.read())
+        authenticated = len(found) != 0
+    if authenticated:
+      outputs.append(f"[yellow]{ key } is already authenticated![reset]")
+      continue
+    if value is not None:
+      with open(path.expanduser(rc_path), "a") as rc_output:
+        rc_output.write(f"export { key }={ value }\n")
+        write_count += 1
+  if write_count > 0:
+    outputs.append("[green]Success!")
+    outputs.append(f"In order to activate authentication, run the following command:")
+    outputs.append(f"    [blue]source { path.expanduser(rc_path) }[reset]")
+  list(map(rich_print, outputs))
 
 
 __all__ = ("auth",)
