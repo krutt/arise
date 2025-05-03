ee3a07dbdd9e3162ac468a5eed691c0df1c94c48
Sitt Guruvanich <aekazitt@gmail.com>
2025-05-03 14:46:16 +0700

    chore: remove usage of Any; effectuate map call

--- src/arise/shadows/igris.py
+++ src/arise/shadows/igris.py
@@ -14,7 +14,8 @@
 from collections import deque
 from math import floor
 from re import search
-from typing import Any, Deque, Generator, Optional, Tuple, Union
+from textwrap import wrap
+from typing import Deque, Dict, Generator, Literal, Optional, Tuple, Union
 
 ### Third-party packages ###
 from rich.box import MINIMAL
@@ -35,12 +36,19 @@ class Igris(Progress):
   def get_renderable(self) -> Union[ConsoleRenderable, RichCast, str]:
     return Group(self.table, *self.get_renderables())
 
-  def get_renderables(self) -> Generator[Table, Any, Any]:
+  def get_renderables(self) -> Generator[Table, None, None]:
     for task in self.tasks:
       if task.fields.get("progress_type") == "build":
         image_name: str = task.description or "undefined"
-        if task.completed <= 0:
+        if task.completed < 0:
           self.columns = (f"[red bold]Build unsuccessful for <Image '{image_name}'>.",)
+        elif task.completed == 0:
+          self.columns = (
+            "Preparing to build [magenta_blue]<Image [reset]'[green]"
+            f"{ image_name }[reset]'[magenta_blue]>[reset].",
+            "".ljust(9),
+            BarColumn(),
+          )
         elif task.completed > 0 and task.completed < 100:
           self.columns = (
             f"Building <[bright_magenta]Image [green]'{image_name}'[reset]>…",
@@ -49,13 +57,20 @@ class Igris(Progress):
           )
         else:
           self.columns = (
-            f"[blue]Built <[bright_magenta]Image [green]'{image_name}'[reset]>[blue] successfully.[reset]",
+            f"[blue]Built <[bright_magenta]Image [green]'{image_name}'[reset]>"
+            "[blue] successfully.[reset]",
+            "".ljust(0),
+            BarColumn()
           )
       elif task.fields.get("progress_type") == "primary":
         self.columns = ("Build specified images:".ljust(42), BarColumn())
       yield self.make_tasks_table([task])
 
-  def progress_build(self, chunk: Generator, task_id: int) -> None:
+  def progress_build(
+    self,
+    chunk: Generator[Dict[Literal["stream", "error"], str], None, None],
+    task_id: TaskID
+  ) -> None:
     for line in chunk:
       if "stream" in line:
         stream: str = line.pop("stream").strip()
@@ -63,7 +78,7 @@ class Igris(Progress):
         if step is not None:
           divided: int = int(step.group("divided"))
           divisor: int = int(step.group("divisor"))
-          self.update(TaskID(task_id), completed=floor(divided / divisor * 100))
+          self.update(task_id, completed=floor(divided / divisor * 100))
         self.update_table(stream)
       elif "error" in line:
         self.update_table(line.pop("error").strip())
@@ -72,8 +87,7 @@ class Igris(Progress):
     if row is not None:
       self.rows.append(row)
     table: Table = Table(box=MINIMAL, show_lines=False, show_header=False)
-    for row_cell in self.rows:
-      table.add_row(row_cell[0:92].ljust(92), style="grey50")
+    list(map(lambda row: table.add_row("\n".join(wrap(row, width=92)), style="grey50"), self.rows))
     self.table = table
 
 
